16858d55458c817fd93bf71b9311090ad190fa79
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-09-15 10:15:12 +0200

    Step 13: Program.cs with ConfigureServices refactor

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -3,32 +3,46 @@ using api.Etc;
 using dataccess;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<AppOptions>(provider =>
-{
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    var appOptions = new AppOptions();
-    configuration.GetSection(nameof(AppOptions)).Bind(appOptions);
-    return appOptions;
-});
-builder.Services.AddDbContext<MyDbContext>((services, options) =>
-{
-    options.UseNpgsql(services.GetRequiredService<AppOptions>().Db);
-});
-builder.Services.AddControllers();
-builder.Services.AddOpenApiDocument();
-builder.Services.AddCors();
-builder.Services.AddScoped<ILibraryService, LibraryService>();
 
-var app = builder.Build();
+public class Program
+{
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        
+        services.AddSingleton<AppOptions>(provider =>
+        {
+            var configuration = provider.GetRequiredService<IConfiguration>();
+            var appOptions = new AppOptions();
+            configuration.GetSection(nameof(AppOptions)).Bind(appOptions);
+            return appOptions;
+        });
+        services.AddDbContext<MyDbContext>((services, options) =>
+        {
+            options.UseNpgsql(services.GetRequiredService<AppOptions>().Db);
+        });
+        services.AddControllers();
+        services.AddOpenApiDocument();
+        services.AddCors();
+        services.AddScoped<ILibraryService, LibraryService>();
 
-var appOptions = app.Services.GetRequiredService<AppOptions>();
+    }
+
+    public static void Main()
+    {
+        var builder = WebApplication.CreateBuilder();
+        ConfigureServices(builder.Services);
+        var app = builder.Build();
+        var appOptions = app.Services.GetRequiredService<AppOptions>();
 //Here im just checking that I can get the "Db" connection string - it throws exception if not minimum 1 length
-Validator.ValidateObject(appOptions, new ValidationContext(appOptions), true);
+        Validator.ValidateObject(appOptions, new ValidationContext(appOptions), true);
 
-app.UseOpenApi();
-app.UseSwaggerUi();
-app.UseCors(config => config.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().SetIsOriginAllowed(x => true));
-app.MapControllers();
-app.GenerateApiClientsFromOpenApi("/../../client/src/generated-client.ts").GetAwaiter().GetResult();
-app.Run();
+        app.UseOpenApi();
+        app.UseSwaggerUi();
+        app.UseCors(config => config.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().SetIsOriginAllowed(x => true));
+        app.MapControllers();
+        app.GenerateApiClientsFromOpenApi("/../../client/src/generated-client.ts").GetAwaiter().GetResult();
+        app.Run();
+
+    }
+}
+
