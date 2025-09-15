37dab2a5117a38be133e8978c3eb50fa2789dbfc
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-09-15 11:40:21 +0200

    routes for overviews

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,29 +4,54 @@ import {DevTools} from "jotai-devtools";
 import 'jotai-devtools'
 import {useEffect} from "react";
 import {libraryApi} from "./api-clients.ts";
+import {useAtom} from "jotai/react/useAtom";
+import {AllAuthorsAtom, AllBooksAtom, AllGenresAtom} from "./atoms/atoms.ts";
+import Books from "./Components/Books.tsx";
+import Authors from "./Components/Authors.tsx";
+import Genres from "./Components/Genres.tsx";
 
 
 function App() {
-    
+
+    const [, setAuthors] = useAtom(AllAuthorsAtom)
+    const [, setBooks] = useAtom(AllBooksAtom)
+    const [, setGenres] = useAtom(AllGenresAtom)
+
     useEffect(() => {
-         libraryApi.getAuthors().then(res => {
-            console.log(res)
-        }).catch(e => {
-            console.log(e)
-        })
+        initializeData();
     }, [])
 
-  return (
-    <>
-      <RouterProvider router={createBrowserRouter([
-          {
-              path: '',
-              element: <Home />
-          }
-      ])} />
-        <DevTools />
-    </>
-  )
+    async function initializeData() {
+        setAuthors(await libraryApi.getAuthors());
+        setBooks(await libraryApi.getBooks())
+        setGenres(await libraryApi.getGenres())
+    }
+
+    return (
+        <>
+            <RouterProvider router={createBrowserRouter([
+                {
+                    path: '',
+                    element: <Home/>,
+                    children: [
+                        {
+                            path: 'books',
+                            element: <Books/>
+                        },
+                        {
+                            path: 'authors',
+                            element: <Authors/>
+                        },
+                        {
+                            path: 'genres',
+                            element: <Genres/>
+                        }
+                    ]
+                }
+            ])}/>
+            <DevTools/>
+        </>
+    )
 }
 
 export default App
