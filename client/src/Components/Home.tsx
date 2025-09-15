f92b1f000957dc276adaf64609d2b1b77341d451
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-09-15 10:40:22 +0200

    step 17: add daisyui

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,3 +1,6 @@
 export default function Home() {
-    return <>Home component</>
+    return <>Home component
+    {/*    here im just testing daisyui theme after step 17*/}
+    <button className="btn btn-primary">Click me</button>
+    </>
 }
